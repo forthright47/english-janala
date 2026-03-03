9bfa14fcddb687b184527756a69fc2cdbdde7490
forthright47 <k.tuhinjaman@gmail.com>
2026-03-03 19:56:26 +0600

    design dynamic cards for words and display cards by fetching the API to UI

--- script/script.js
+++ script/script.js
@@ -1,26 +1,54 @@
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
-    .then((res) => res.json()) // promise of json data
-    .then((json) => displayLessons(json.data));
+        .then((res) => res.json()) // promise of json data
+        .then((json) => displayLessons(json.data));
 }
 
 const displayLessons = (lessons) => {
     // 1. get the container and empty
     const levelContainer = document.getElementById("level-container");
     levelContainer.innerHTML = "";
-    
+
     // 2. get into every lessons
-    lessons.forEach(lesson =>{
+    lessons.forEach(lesson => {
         // 3. create HTML element
         const btnDiv = document.createElement("div");
         btnDiv.innerHTML = `
-        <button class="btn btn-outline btn-primary">
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
             <i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}
         </button>
-        `
+        `;
         // 4. append to the container
         levelContainer.appendChild(btnDiv);
     })
 }
 
 loadLessons();
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => displayLevelWord(data.data));
+}
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML = "";
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word}</h2>
+            <p class="font-semibold text-sm">Meaning / Pronounciation</p>
+            <div class="font-bangla font-medium text-2xl">"${word.meaning} / ${word.pronunciation}"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `;
+        wordContainer.appendChild(card);
+    })
+}
